baab65e78e2f0af2952f411e67f26bee92a4580e
HectorRC920 <hector.rubio.920@gmail.com>
2025-01-23 08:22:14 -0600

    Start frontend app

--- src/routes/filesRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import FilesController from '../controllers/filesController';
-
-const router = express.Router();
-
-router.get('/', FilesController.getFiles);
-
-export default router;
