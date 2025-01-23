4124558657341f18026c39cb41c79cea6a7ae5f4
HectorRC920 <hector.rubio.920@gmail.com>
2025-01-23 08:21:33 -0600

    start frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="bg-light vh-100 d-flex justify-content-center align-items-center">
+            <h1 className="text-primary">Hello Toolbox!</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public', ),
+        },
+        open: true,
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    mode: 'development',
+};
