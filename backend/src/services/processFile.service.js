eb519b85b4659a4715780ee4190debf3c13bc221
HectorRC920 <hector.rubio.920@gmail.com>
2025-01-23 20:44:33 -0600

    remove useless method and library

--- backend/src/services/processFile.service.js
+++ backend/src/services/processFile.service.js
@@ -1,6 +1,4 @@
 import { AxiosWrapper } from "../wrapper/axios.wrapper.js"; 
-import csvParser from "csv-parser"; 
-import { Readable } from "stream"; 
 
 /**
  * Service class to process CSV files fetched from an API.
@@ -27,50 +25,6 @@ export default class ProcessFileService {
     return ProcessFileService._instance; // Return the singleton instance
   }
 
-  /**
-   * Processes a CSV file by streaming and parsing its content.
-   * @param {string} file - The file name to be processed.
-   * @returns {Promise<Object>} An object containing the file name and valid lines.
-   * This method is not used because the challenge does not allow use external libraries
-   */
-  async processCSVFile(file) {
-    const validLines = []; // Array to store valid lines
-    const axiosRequestObj = {
-      url: `https://echo-serv.tbxnet.com/v1/secret/file/${file}`, // API endpoint
-      method: 'GET', 
-      responseType: 'text' 
-    };
-
-    try {
-      const response = await this.axios.invokeApi(axiosRequestObj); 
-      const stream = Readable.from(response); 
-
-      const parser = stream.pipe(csvParser()); // Parse the stream as CSV
-
-      for await (const row of parser) {
-        const { text, number, hex } = row; // Extract fields from the row
-
-        // Validate the row and push it to validLines if valid
-        if (text && number && hex) {
-          validLines.push({ text, number, hex });
-        }
-      }
-
-      return {
-        file,
-        lines: validLines, 
-      };
-    } catch (error) {
-      // Handle errors (Axios-specific or general)
-      if (error.isAxiosError) {
-        console.error(`Error processing file ${file}:`, error.message);
-      } else {
-        console.error(`Unexpected error processing file ${file}:`, error);
-      }
-    }
-
-    return {}; // Return an empty object in case of failure
-  }
 
   /**
    * Processes a CSV file using a regex approach to extract and validate lines.
